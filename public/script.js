2bdc4736375f5516dec3a43ce54ca624e0360ab0
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-09-28 22:53:24 -0400

    deploy ready

--- public/script.js
+++ public/script.js
@@ -1,42 +1,82 @@
-const collapseEl = $(".content-wrap");
-const collapseBody = $(".collapsible-body");
+const data = [
+  {
+    name: 'Free To Me',
+    image: 'img/movie.png',
+    site: "https://crystajeffcoat.github.io/MovieFinder/",
+    git: "https://github.com/CrystaJeffcoat/MovieFinder.git"
+  },
+  {
+    name: 'Todo Calendar',
+    image: 'img/schedule.png',
+    site: "https://crystajeffcoat.github.io/Schedule-Calendar-Todo-List/",
+    git: "https://github.com/CrystaJeffcoat/Schedule-Calendar-Todo-List.git"
+  },
+  {
+    name: 'Weather Dashboard',
+    image: 'img/weather.png',
+    site: "https://crystajeffcoat.github.io/Weather-Dashboard/",
+    git: "https://github.com/CrystaJeffcoat/Weather-Dashboard.git"
+  },
+  {
+    name: 'Chart Master',
+    image: 'img/chartMaster.png',
+    site: 'https://still-anchorage-95326.herokuapp.com/',
+    git: 'https://github.com/blkmtnmouth/Chartmaster'
+  },
+  {
+    name: 'Eat da Burger',
+    image: 'img/burger.png',
+    site: 'https://github.com/CrystaJeffcoat/Eat-da-burger',
+    git: 'https://github.com/CrystaJeffcoat/Eat-da-burger'
+  },
+  {
+    name: 'Notepad',
+    image: 'img/notepad.png',
+    site: 'https://intense-sea-80554.herokuapp.com/notes',
+    git: 'https://github.com/CrystaJeffcoat/NotePad'
+  }
+];
 
-const projInfoArr = {
-  FreeToMe: [
-    {
-      site: "https://crystajeffcoat.github.io/MovieFinder/",
-      git: "https://github.com/CrystaJeffcoat/MovieFinder.git"
-    }
-  ],
-  toDoCalendar: [
-    {
-      site: "https://crystajeffcoat.github.io/Schedule-Calendar-Todo-List/",
-      git: "https://github.com/CrystaJeffcoat/Schedule-Calendar-Todo-List.git"
-    }
-  ],
-  weatherDash: [
-    {
-      site: "https://crystajeffcoat.github.io/Weather-Dashboard/",
-      git: "https://github.com/CrystaJeffcoat/Weather-Dashboard.git"
-    }
-  ],
-  ChartMaster: [
-    {
-      site: 'https://still-anchorage-95326.herokuapp.com/',
-      git: 'https://github.com/blkmtnmouth/Chartmaster'
-    }
-  ]
+function createEl(htmlString = "", className) {
+  const el = document.createElement(htmlString);
+  if (className) {
+    el.setAttribute("class", className);
+  }
+  return el;
 };
 
-console.log(projInfoArr.FreeToMe.site)
+createCards(data);
 
-$(document).ready(function () {
-  collapseEl.hover(function () {
-    collapseBody.stop().slideDown("slow");
-  }, function () {
-    collapseBody.stop().slideUp("slow");
+function createCards(data) {
+  const container = document.querySelector(".container");
+  container.innerHTML = "";
+  let lastRow;
+  const row = createEl("div", "row row-flex");
+
+  return data.forEach(function(image, index) {
+    const col = createEl("div", "col m4");
+    col.appendChild(createCard(image));
+    if (index % 3 === 0) {
+      row.appendChild(col);
+      container.appendChild(row);
+      lastRow = row;
+    }
+
+    return lastRow.appendChild(col);
   });
-});
+}
+
+
+function createCard(image) {
+  //const card = createEl("div", "card small");
+  const imageContainer = createEl("div", "card card-flex");
+  const img = createEl("img", 'card-image responsive-img z-depth-4');
+  img.setAttribute("src", image.image);
+
+  imageContainer.append(img);
+
+  return imageContainer;
+};
 
 $(document).click(function () {
   console.log(event.target);
